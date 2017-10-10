0f48360c0e7eb332629b6b6402aa2907c250fa39
xugy0926 <young_xu@qq.com>
2017-10-10 19:28:55 +0800

    add first-app-sample-8

---
+++ first-app-sample-8/route.page.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+res.render('index', { title: 'Express' });
+});
+
+/* GET posts page. */
+router.get('/posts', function(req, res, next) {
+res.render('posts', { title: 'posts'} );
+});
+
+/* GET posts edit page. */
+router.get('/posts/create', function(req, res, next) {
+  res.render('create');
+});
+
+module.exports = router;
