6828ab18b6ae34128d595be14ca0c54ca6ec4699
xugy0926 <young_xu@qq.com>
2017-10-23 17:14:57 +0800

    add first-app-sample-11

---
+++ first-app-sample-11/route.api.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var router = express.Router();
+var PostModel = require('./models/post');
+
+/* GET users lists. */
+router.get('/users', function(req, res, next) {
+res.send('respond with a resource');
+});
+
+/* GET posts lists */
+router.get('/posts', function (req, res, next) {
+  PostModel.find({}, {}, function (err, posts) {
+    if (err) {
+      res.json({ success: false });
+      return;
+    }
+
+    res.json({ success: true, postsList: posts });
+  });
+});
+
+/* POST posts */
+router.post('/posts', function (req, res, next) {
+  console.log(req.body);
+  var title = req.body.title;
+  var content = req.body.content;
+
+  var post = new PostModel();
+  post.title = title;
+  post.content = content;
+  post.save(function (err) {
+    if (err) {
+      console.log(err);
+      res.json({success: false});
+      return;
+    }
+    res.json({success: true});
+  });
+});
+
+module.exports = router;
