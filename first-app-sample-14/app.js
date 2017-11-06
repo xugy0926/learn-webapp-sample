63ef86cc8b708946e0c1dfbb268701ba0841be3f
xugy0926 <young_xu@qq.com>
2017-11-06 15:52:28 +0800

    update

--- first-app-sample-14/app.js
+++ first-app-sample-14/app.js
@@ -27,7 +27,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', page);
-app.use('/api', api);
+app.use('/api/v1', api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
