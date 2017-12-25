7bccb63e0e2176b817fe9822149b68d047be1ebe
xugaoyang <i@xugaoyang.com>
2017-12-25 22:24:39 +0800

    Update route.api.js

--- first-app-sample-23/src/route.api.js
+++ first-app-sample-23/src/route.api.js
@@ -105,7 +105,7 @@ router.post('/signin', function(req, res, next) {
         {
           _id: user._id,
           name: user.name,
-          isAdmin: user.loginname === config.admin ? true : false,
+          isAdmin: user.loginname === config.admin,
           exp: moment().add('days', 30).valueOf(),
         },
         config.jwtSecret
